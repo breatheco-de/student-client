17e92d031a6a849361ff5de646d849679fb1cbd7
Alejandro Sanchez <aalejo@gmail.com>
2018-04-08 23:10:20 +0000

    utf-8

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,15 @@ const Dotenv = require('dotenv-webpack');
 module.exports = merge(common, {
     plugins: [
         new UglifyJSPlugin({
-            sourceMap: true
+            sourceMap: true,
+            uglifyOptions: {
+                ecma: 8,
+                warnings: false,
+                output: {
+                    comments: false,
+                    beautify: false,
+                }
+            }
         }),
         new Dotenv({
             path: './.prod.env'
