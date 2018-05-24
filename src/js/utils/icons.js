6527d120e2285e87f6421738b22ebfe131938df5
Alejandro Sanchez <aalejo@gmail.com>
2018-05-23 23:31:40 -0400

    Update icons.js

--- src/js/utils/icons.js
+++ src/js/utils/icons.js
@@ -1,5 +1,14 @@
 /* global FontAwesomeConfig */
+
+/**
+ * 1) First you have to import the @fortawesome/fontawesome library
+*/
 import fontawesome from '@fortawesome/fontawesome';
+
+
+/**
+ * 2) Then you have to import every icon that you will use
+*/
 import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
 import faSync from '@fortawesome/fontawesome-free-solid/faSync';
 import faEllipsisV from '@fortawesome/fontawesome-free-solid/faEllipsisV';
@@ -11,10 +20,23 @@ import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
 import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
 import faExternalLinkAlt from '@fortawesome/fontawesome-free-solid/faExternalLinkAlt';
 
+/**
+ * 3) Tell font-awesome that you want to replace your icons with SVGs (recomended for performance)
+*/
 fontawesome.config = {
   autoReplaceSvg: 'nest'
 }
+
+/**
+ * 4) Add the icons into the font-awesome library
+*/
 fontawesome.library.add(
     faCheck, faGraduationCap, faPlay, faSpinner, faSearch, faGithub,
     faCog, faSync, faEllipsisV, faExternalLinkAlt
 );
+
+
+/**
+ * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
+ *    <i class="fas fa-check"></i>
+*/
