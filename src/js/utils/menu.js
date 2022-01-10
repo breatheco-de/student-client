cd3bab72dce770ed5eaaf95e4bb82e1201dc9ea4
xenogenesis20 <xenogenesis20@outlook.com>
2022-01-10 17:27:17 +0000

    trying to push to my PR.

--- src/js/utils/menu.js
+++ src/js/utils/menu.js
@@ -5,15 +5,6 @@ import SearchMenu from '../components/menus/SearchMenu';
 import IFrameView from '../views/panel/IFrameView';
 import { Session } from 'bc-react-session';
 
-let session = Session.getSession();
-console.log(session);
-//  What the iframe URL must look like if student is in Miami
-//  https://mentor.breatheco.de/academy/downtown-miami/service/geekpal/mentor?token=48a661fac55064bcb61f9d88f08a01ac22311fc3
-let token = session.payload.token
-let academySlug = session ? session.payload.currentCohort.cohort.academy.slug : ''
-let newIframUrl = `https://mentor.breatheco.de/academy/${academySlug}/service/geekpal/mentor?token=${token}`
-const oldIframeUrl = "https://4geeks.setmore.com"
-
 export const getCurrentPath = (pathname = null, hash = null) => {
     if (!pathname) pathname = window.location.pathname;
     if (!hash) hash = window.location.hash;
@@ -23,6 +14,7 @@ export const getCurrentPath = (pathname = null, hash = null) => {
     const dayMatch = pathname.match(dayRegex);
     let menuRegex = /menu=(.+)$/;
     const menuMatch = hash.match(menuRegex);
+
     const menu = (!menuMatch) ? null : menuMatch[1];
     const course = (!dayMatch) ? null : dayMatch[1];
     const day = (!dayMatch) ? null : dayMatch[2];
@@ -32,7 +24,6 @@ export const getCurrentPath = (pathname = null, hash = null) => {
     return { day, type, course, view, pathname, hash, menu };
 };
 export const menuModes = {
-
     home: [
         { slug: "home", label: "BreatheCode" }
     ],
@@ -69,17 +60,23 @@ export const menuModes = {
                         size: 370
                     },
                     {
-                        slug: "make-gp-appointment",
-                        label: "GeekPal Appointment",
-                        icon: "fas fa-calendar-alt",
-                        component: () => <IFrameView src={academySlug == "downtown-miami" ? newIframUrl : oldIframeUrl} style={{ width: "100%", marginLeft: "-1px" }} />,
-                        size: 370
-                    },
-                    {
                         slug: "make-appointment",
                         label: "Appointments",
                         icon: "fas fa-calendar-alt",
-                        component: () => <IFrameView src={oldIframeUrl} style={{ width: "100%", marginLeft: "-1px" }} />,
+                        component: () => {
+                            //  What the iframe URL must look like if student is in Miami
+                            //  https://mentor.breatheco.de/academy/downtown-miami/service/geekpal/mentor?token=2538c67e585ab65e4845a6843ae1c689d04c1192
+
+                            let session = Session.getSession();
+                            console.log(session);
+                            let token = session.payload.token ? session.payload.token : ''
+                            let academySlug = session.payload.currentCohort ? session.payload.currentCohort.cohort.academy.slug : ''
+                            let newIframUrl = token && academySlug && `https://mentor.breatheco.de/academy/${academySlug}/service/geekpal/mentor?token=${token}`
+                            const oldIframeUrl = "https://4geeks.setmore.com"
+
+                            return (<IFrameView src={academySlug === 'downtown-miami' ? newIframUrl : oldIframeUrl} style={{ width: "100%", marginLeft: "-1px" }} />
+                            )
+                        },
                         size: 370
                     }
                 ]
