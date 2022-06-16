7c7cd139c6267a4625e2e44b3dc30e0be7f650bf
Alejandro Sanchez <aalejo@gmail.com>
2022-06-15 20:26:23 -0400

    Update AssignmentView.js

--- src/js/views/panel/AssignmentView.js
+++ src/js/views/panel/AssignmentView.js
@@ -15,7 +15,7 @@ export default class LessonView extends Flux.View {
   }
 
   render() {
-    const url = "https://breatheco.de/project/"+this.props.match.params.assignment_slug+'?plain=true&access_token='+this.state.token;
+    const url = "https://project.breatheco.de/?slug="+this.props.match.params.assignment_slug;
     return (
       <Panel padding={false}>
         <Loading show={this.state.loading} />
